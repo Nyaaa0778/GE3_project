c0367edb837e7ed54c812838247fbee9ca0018fe
Nyaaa0778 <k024g0093@g.neec.ac.jp>
2025-11-16 14:35:43 +0900

    共通描画部分を移植

--- project/engine/2d/SpriteCommon.h
+++ project/engine/2d/SpriteCommon.h
@@ -1,7 +1,45 @@
 #pragma once
+#include <wrl.h>
+#include <d3d12.h>
+
+class DirectXCommon;
+
 class SpriteCommon {
+public:
   /// <summary>
   /// 初期化
   /// </summary>
-  void Initialize();
+  /// <param name="dxCommon">DirectXCommonのポインタ</param>
+  void Initialize(DirectXCommon *dxCommon);
+
+  /// <summary>
+  /// 共通描画設定
+  /// </summary>
+  void SetupCommonRenderState();
+
+public:
+  DirectXCommon *GetDxCommon() const { return dxCommon_; }
+
+private:
+  /// <summary>
+  /// ルートシグネチャの生成
+  /// </summary>
+  void CreateRootSignature();
+
+  /// <summary>
+  /// グラフィックスパイプラインの生成
+  /// </summary>
+  void CreateGraphicsPipeline();
+
+private:
+  // namespace
+  template <class InterfaceType>
+  using ComPtr = Microsoft::WRL::ComPtr<InterfaceType>;
+
+  // DirectXCommonのポインタ
+  DirectXCommon *dxCommon_ = nullptr;
+
+  ComPtr<ID3D12RootSignature> rootSignature_ = nullptr;
+
+  ComPtr<ID3D12PipelineState> graphicsPipelineState_ = nullptr;
 };
